35b69f88ded2d5c75211325cc5905183c5eb44cb
lautajam <lautajamdev@gmail.com>
2023-08-26 21:49:06 -0300

    delete a things

--- create_files/create_files.py
+++ create_files/create_files.py
@@ -13,8 +13,4 @@ path = input(print("Enter the path where you want to create the files: "))
 for i in range(1, int(number_file) + 1):
     file = open(path + "/" + name_file + str(i) + extension, "w")
     file.close()
-    
-# generate a coment with explain the script
-
-print("This script creates files with the name, number and extension that the user wants.")
 
