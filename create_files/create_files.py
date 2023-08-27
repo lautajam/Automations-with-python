b866bf7dd092593ecbe63975bb28c51de5a681d6
lautajam <lautajamdev@gmail.com>
2023-08-26 21:46:17 -0300

    This script creates files with the name, number and extension that the user wants

---
+++ create_files/create_files.py
@@ -0,0 +1,20 @@
+"""This script creates files with the name, number and extension that the user wants."""
+
+# Ask the user for the name, number and extension of the files. Also ask for the path where you want to create the files.
+name_file = input(print("Enter the name of the file: "))
+
+number_file = input(print("Enter the number of the file: "))
+
+extension = input(print("Enter the extension of the files: "))
+
+path = input(print("Enter the path where you want to create the files: "))
+
+# Create the files with the name, number and extension that the user wants.
+for i in range(1, int(number_file) + 1):
+    file = open(path + "/" + name_file + str(i) + extension, "w")
+    file.close()
+    
+# generate a coment with explain the script
+
+print("This script creates files with the name, number and extension that the user wants.")
+
